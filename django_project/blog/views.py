6f365b8bd9c5486d1abb99d0715a685f7ebc5e16
Abeer <abeer.sewak@gmail.com>
2019-03-05 16:05:05 -0600

    Removed the dummy data and made the database connections

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'AbeerGS',
-        'title': 'Blog Post 1',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et sint, eveniet magnam voluptas labore fugiat aliquam ex inventore in quaerat, explicabo, beatae sunt porro. Iste aspernatur quam sint ea quibusdam.',
-        'date_posted': 'March 4, 2019'
-    },
-    {
-        'author': 'Test User 1',
-        'title': 'Blog Post 2',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, deleniti!',
-        'date_posted': 'March 7, 2019'
-    }
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
