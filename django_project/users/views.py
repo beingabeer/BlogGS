fa5ebaf09b838af4706b2bdd4e5f59b8bc971f4b
Abeer <abeer.sewak@gmail.com>
2019-03-06 18:16:18 -0600

    Created a profile model with a one to one relation with the user

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You can now log in')
+            messages.success(request, f'Hi {username}! Your account has been created! You can now log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
