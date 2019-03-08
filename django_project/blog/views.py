ae40a283dd00298d8eb5e31fd04a5ac3aad77f73
Abeer <abeer.sewak@gmail.com>
2019-03-07 20:14:53 -0600

    Added the Create and Update post functionality

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
 
 
 class PostListView(ListView):
@@ -17,8 +21,18 @@ class PostListView(ListView):
     ordering = ['-date_posted']
 
 
+# naming convention -> app/model_viewtype.html  ->  blog/post_detail.html
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
