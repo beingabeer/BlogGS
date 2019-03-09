91a5558df5b1b855199f1b2b197a7d68649209cf
Abeer <abeer.sewak@gmail.com>
2019-03-08 23:56:18 -0600

    fixed the pagination logic

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -43,7 +43,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
-    success_url = '/'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
