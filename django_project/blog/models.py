3679c54888c1fcb0a8304dfd93f8589127b8ffa7
Abeer <abeer.sewak@gmail.com>
2019-03-13 22:06:07 -0500

    Made the content section optional

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(blank=True, upload_to='blog_pics')
